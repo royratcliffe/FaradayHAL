1f730e1020215083bdfef4f63879a9ba825c8283
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-11-08 17:56:37 +0000

    First commit

---
+++ FaradayHAL/EncodeJSON.swift
@@ -0,0 +1,60 @@
+// FaradayHAL EncodeJSON.swift
+//
+// Copyright © 2015, Roy Ratcliffe, Pioneering Software, United Kingdom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+import Faraday
+import HypertextApplicationLanguage
+
+/// Encodes a hypertext application language (HAL) representation body to
+/// JSON. Does nothing if the body is not a HAL representation.
+public class EncodeJSON: Faraday.Middleware {
+
+  public override func call(env: Env) -> Response {
+    guard let request = env.request else {
+      return super.call(env)
+    }
+    guard let body = request.body as? Representation else {
+      return super.call(env)
+    }
+    guard request.headers["Content-Type"] == nil else {
+      return super.call(env)
+    }
+    let object = NSDictionaryRepresentationRenderer.render(body)
+    if let data = try? NSJSONSerialization.dataWithJSONObject(object, options: [.PrettyPrinted]) {
+      request.headers["Content-Type"] = "application/hal+json"
+      request.body = data
+    }
+    return app(env)
+  }
+
+  public class Handler: RackHandler {
+
+    public init() {}
+
+    public func build(app: App) -> Middleware {
+      return EncodeJSON(app: app)
+    }
+    
+  }
+  
+}
---
+++ FaradayHALTests/FaradayHALTests.swift
@@ -0,0 +1,112 @@
+// FaradayHALTests FaradayHALTests.swift
+//
+// Copyright © 2015, Roy Ratcliffe, Pioneering Software, United Kingdom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+import XCTest
+@testable import FaradayHAL
+
+import Faraday
+import HypertextApplicationLanguage
+
+class FaradayHALTests: XCTestCase {
+
+  /// Encapsulates a fake adapter that merely remembers the request-response
+  /// environments so that tests can subsequently save the finished responses
+  /// artificially.
+  class NothingAdapter: Adapter {
+
+    static var envs = [Env]()
+
+    /// The implementation instantiates a temporary `NothingAdapter` in order to
+    /// save the responses. Only adapter instances know how to save and finish
+    /// them. The temporary adapter does nothing, nor is it ever invoked as a
+    /// member of a middleware stack. It just saves the responses just because
+    /// it knows how.
+    static func saveResponses(status: Int, body: Body?, headers: Headers) {
+      let adapter = NothingAdapter { env -> Response in
+        return Response()
+      }
+      while let env = envs.popLast() {
+        adapter.saveResponse(env, status: status, body: body, headers: headers)
+      }
+    }
+
+    override func call(env: Env) -> Response {
+      NothingAdapter.envs.append(env)
+      return app(env)
+    }
+
+    class Handler: RackHandler {
+
+      func build(app: App) -> Middleware {
+        return NothingAdapter(app: app)
+      }
+
+    }
+
+  }
+
+  func testNothingAdapter() {
+    // given
+    let connection = Connection()
+    connection.use(FaradayHAL.EncodeJSON.Handler())
+    connection.use(Faraday.Logger.Handler())
+    connection.use(NothingAdapter.Handler())
+    // when
+    connection.get().onComplete { env in
+      XCTAssertNotNil(env.request)
+      XCTAssertNotNil(env.response)
+      XCTAssertEqual(env.request?.method, "GET")
+      XCTAssertEqual(env.response?.status, 0)
+    }
+    // then
+    NothingAdapter.saveResponses(0, body: nil, headers: Headers())
+  }
+
+  func testEncodeJSON() {
+    // given
+    let connection = Connection()
+    connection.use(FaradayHAL.EncodeJSON.Handler())
+    connection.use(Faraday.Logger.Handler())
+    connection.use(NothingAdapter.Handler())
+    // when
+    let response = connection.post { request in
+      let representation = Representation()
+      representation.withLink(Link(rel: Link.SelfRel, href: "http://localhost/path/to/self"))
+      request.body = representation
+    }
+    response.onComplete { env in
+      XCTAssertNotNil(env.request)
+      XCTAssertNotNil(env.response)
+
+      XCTAssertEqual(env.request?.method, "POST")
+      XCTAssertEqual(env.response?.status, 0)
+
+      XCTAssertNotNil(env.request?.body)
+      XCTAssertNotNil(env.request?.body as? NSData)
+    }
+    // then
+    NothingAdapter.saveResponses(0, body: nil, headers: Headers())
+  }
+
+}
