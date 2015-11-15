7c78243c80127d9f81a3afe91eeea43cd2d3eca3
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-11-15 06:03:55 +0000

    DecodeJSON.Handler needs a public initialiser

--- FaradayHAL/DecodeJSON.swift
+++ FaradayHAL/DecodeJSON.swift
@@ -54,6 +54,8 @@ public class DecodeJSON: Response.Middleware {
 
   public class Handler: RackHandler {
 
+    public init() {}
+
     public func build(app: App) -> Middleware {
       return DecodeJSON(app: app)
     }
